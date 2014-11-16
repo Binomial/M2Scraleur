953f01a90dde8ab030cf6295980f247dc25e8bbf
Binomial <jeremy_sc_hneiter@hotmail.fr>
2014-11-16 20:35:09 +0100

    test technique JB

--- src/cpp/main/main.cpp
+++ src/cpp/main/main.cpp
@@ -5,17 +5,23 @@
  * Created on 30 octobre 2014, 21:24
  */
 
-#include <cstdlib>
-#include "../../include/menu/MenuPrinc.h"
+#include "../../include/Dictionnary.h"
+#include "../../include/FindWord.h"
 using namespace std;
 
 int main() {
     
- /*   MenuPrinc menu;
-    menu.menu();
-  * */
-    Anagram an;
-    an.fileToMap("dico/dicoFr1.dc");
-    an.generateMap();
+    Dictionnary dict;
+    dict.importDictionnary();
+    
+    FindWord fw(dict.getMap());
+    string letters;
+    
+    cout << "Saisir lettres : ";
+    cin >> letters;
+    
+    fw.permut(letters);
+    fw.displaySolution();
+    
     return 0;
 }
